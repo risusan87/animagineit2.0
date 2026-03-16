bd58d251868386ddcef4a2f2b3901ede087e0766
rkoba0020 <rkoba0020@gmail.com>
2026-03-16 09:37:30 -0400

    Add Image model

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -8,3 +8,9 @@ class AppConfiguration(Base):
     id: int = Column(Integer, primary_key=True, index=True)
     key: str = Column(String(255), unique=True, index=True, nullable=False)
     value: str = Column(String(255), nullable=False)
+
+class ImageBlob(Base):
+    __tablename__ = "image_blob"
+    id: int = Column(Integer, primary_key=True, index=True)
+    name: str = Column(String(255), unique=True, index=True, nullable=False)
+    data: bytes = Column(String, nullable=False)
